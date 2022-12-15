1989a0ad07b70ecb70365811ba484ffbbedeb5be
eddie-coding <adityabahuguna007@gmail.com>
2022-12-15 21:03:17 +0530

    Calling the other 2 external Microservice API

--- movie-catalog-service/src/main/java/com/eddie/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/eddie/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +14,21 @@ import org.springframework.web.client.RestTemplate;
 import com.eddie.moviecatalogservice.models.CatalogItem;
 import com.eddie.moviecatalogservice.models.Movie;
 import com.eddie.moviecatalogservice.models.Rating;
+import com.eddie.moviecatalogservice.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        List<Rating> ratings = Arrays.asList(
-            new Rating("1234", 4),
-            new Rating("5678", 3)
-        );
-
-        return ratings.stream().map(rating -> {
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        
+        return ratings.getUserRating().stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         })
